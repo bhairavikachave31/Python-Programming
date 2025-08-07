cd0c57dc1b63190a875dbef5a8237b4e42965bd9
bhairavikachave31 <163982398+bhairavikachave31@users.noreply.github.com>
2025-08-07 12:07:41 +0530

    Add files via upload

---
+++ Mathematical Operations.py
@@ -0,0 +1,7 @@
+#Performing Basic Mathematical Operations
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+print("Addition is: ", a+b)
+print("Subtraction is: ", a-b)
+print("Multiplication is: ", a*b)
+print("Division is: ", a/b)
---
+++ first name.py
@@ -0,0 +1,7 @@
+#Creating Personalized Greeting
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print(full_name)
+print(f"Hey,{full_name} ! Enjoy your first python code here.")
+print(f"Hello,{first_name}! We are here to guide you.")
